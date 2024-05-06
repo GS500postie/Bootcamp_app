741df2cf21bfb6506dcc0093cabf4125db7e5483
John <johnborron665@gmail.com>
2024-05-06 12:36:36 +1000

    finished module 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
-import 'dotenv/config'
+import 'dotenv/config';
+
 
 // https://astro.build/config
 export default defineConfig({
